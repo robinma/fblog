21494cc510ad9ddb66bec285cd35cfd48a30c1a8
robin ma <ahmzj@163.com>
2013-07-16 01:30:39 +0800

    modify ready static pages style

--- server/modules/router.js
+++ server/modules/router.js
@@ -1,52 +1,71 @@
 /**
  * @author robin ma
  */
-var fs=require('fs');
-var path=require('path');
+var fs = require('fs');
+var path = require('path');
+var zlib=require('zlib');
 
-var mime=require('../config/mime').types;
-var config=require('../config/config');
+var mime = require('../config/mime').types;
+var config = require('../config/config');
 
-function route(handle,pathname,response){
-	console.log("About to route a request for "+pathname);
-	if(typeof handle[pathname] === 'function'){
+function route(handle, pathname, request, response) {
+	console.log("About to route a request for " + pathname);
+	if ( typeof handle[pathname] === 'function') {
 		handle[pathname]();
-	}else{
-		staticFiles(pathname,response);
+	} else {
+		staticFiles(pathname, request, response);
 	}
 }
 
-
-var staticFiles=function(pathname,response){
-	var realPath='web'+pathname;
-	var ext=path.extname(realPath);
-	ext=ext?ext.slice(1) : 'unknow';
-	fs.exists(realPath,function(exists){
-		if(!exists){
-			response.writeHead(404,{'Content-Type':mime[ext]});
-			response.write('This request URL '+realPath+' was not found on this server.');
+var staticFiles = function(pathname, request, response) {
+	var realPath = 'web' + pathname;
+	var ext = path.extname(realPath);
+	ext = ext ? ext.slice(1) : 'text/plain';
+	fs.exists(realPath, function(exists) {
+		if (!exists) {
+			response.writeHead(404, {
+				'Content-Type' : 'text/plain'
+			});
+			response.write('This request URL ' + realPath + ' was not found on this server.');
 			response.end();
-		}else{
-			fs.readFile(realPath,function(err,data){
-				if(err){
-					response.writeHead(500,{'Content-Type':mime[ext]});
-					response.end(err);
-				}else{
-					if(ext.match(config.Expires.fileMatch)){
-						var expires=new Date();
-						expires.setTime(expires.getTime()+config.Expires.maxAge*1000);
-						response.setHeader('Expres',expires.toUTCString());
-						response.setHeader('Cache-Control','max-age='+config.Expires.maxAge);
-					}
-					response.writeHead(200,{'Content-Type':mime[ext]});
-					
-					response.write(data,'binary');
-					response.end();
+		} else {
+			fs.stat(realPath, function(err, stat) {
+				var lastModified = stat.mtime.toUTCString();
+				var ifModifiedSince = "If-Modified-Since".toLowerCase();
+				response.setHeader('Last-Modified', lastModified);
+				console.log('Last-Modified', lastModified);
+				if (ext.match(config.Expires.fileMatch)) {
+					var expires = new Date();
+					expires.setTime(expires.getTime() + config.Expires.maxAge);
+					response.setHeader('Expres', expires.toUTCString());
+					response.setHeader('Cache-Control', 'max-age=' + config.Expires.maxAge);
 				}
-			})
+				console.log(request.headers);
+				if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
+					response.writeHead(304, "Not Modified.");
+					response.end();
+				} else {
+					fs.readFile(realPath, 'binary', function(err, data) {
+						if (err) {
+							response.writeHead(500, {
+								'Content-Type' : mime[ext]
+							});
+							response.end(err);
+						} else {
+							response.writeHead(200, {
+								'Content-Type' : mime[ext]
+							});
+
+							response.write(data, 'binary');
+							response.end();
+						}
+					})
+				}
+
+			});
+
 		}
 	});
 }
 
-
-exports.route=route;
+exports.route = route;
