0d40e92d76207f389e9a539daea36e665a6988a5
robin ma <ahmzj@163.com>
2013-08-19 00:21:29 +0800

    test mongoose driver config

--- server/modules/mongodb_connect.js
+++ server/modules/mongodb_connect.js
@@ -2,20 +2,40 @@
  * @author robin ma
  */
 
-var mongodb=require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/blog');
 
-var server = new mongodb.Server('127.0.0.1',27017,{auto_reconnect:true});
-var db=new mongodb.Db('blog',server,{safe:true});
+var db = mongoose.connection;
 
-db.open(function(err,db){
-	if(!err){
-		console.log('========connect db success=======');
-		var clec=db.collection('blog');
-		
-	}else{
-		console.log('--------connect db fail-------------------')
+db.on('err', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('success', '===');
+	//blog schema
+	var blogSchema = new mongoose.Schema({
+		name : String,
+		title : String,
+		id : Number
+	});
+	blogSchema.methods.speak = function() {
+		console.log('my name is : ' + this.name);
 	}
-	
-})
+	var blogModel = db.model('blogtest', blogSchema);
 
+	var blogEntity = new blogModel({
+		name : 'marinArticle',
+		title:'this title is test----',
+		id:1
+	});
+	blogEntity.save();
+	blogEntity.speak();
+	
+	//console.log(blogEntity.find(),'====');
+	//blogEntity.save();
+	blogModel.find(function(err, blog) {
+		if (err)
+			donsole.log('--22---');
+		console.log(blog, '===');
+	});
+
+});
 
