5dee9c3d643bc4ea2b24e28f597e265dd3378514
robin ma <ahmzj@163.com>
2013-09-11 01:52:25 +0800

    go on writer

--- fmvc/application/models/ModelBlogArticle.js
+++ fmvc/application/models/ModelBlogArticle.js
@@ -8,14 +8,33 @@ module.exports = function() {
 	var Schema = mongoose.Schema;
 
 	var Article = new Schema({
-		userid : {type:Number},
+		userid : Number,
 		title : {type:String,max:30},
 		content : {type:String},
-		timestamp : {type:Date,default:Date.now},
+		timestamp : {type:Date,default:Date.now()},
 		images : {type:[String]},
 		visiCount : {type:Number},
 		commCount : {type:Number},
 		classid : {type:Number}
 	});
+	
+	//访问模型
+	
+	var blog=mongoose.model('blog',Article);
+	
+	//实例
+	var dit={userid:1,title:'testname',content:'this is content'};
+	var blogtest=new blog(dit);
+	blogtest.save(function(err){
+		console.log(err);
+		if(err){
+			console.log('------',err);
+		}else{
+			console.log('++++++',err);
+		}
+	});
+	
+	console.log('=====',blogtest);
+	
 
 }
