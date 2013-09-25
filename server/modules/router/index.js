a4f45f3baaf573cf28b1f3ef6af49c41cb13c61a
robin ma <ahmzj@163.com>
2013-09-26 01:27:08 +0800

    edit code

--- server/modules/router/index.js
+++ server/modules/router/index.js
@@ -2,13 +2,25 @@
  * @author jerry
  */
 
-function router(options){
+function Router(options){
 	options=iptions||{};
 	var __=this;
 	this.map={};
 	this.params={};
-	this._params=[];
+	this._params=[]; //Register a param callback `fn` for the given `name`.
 	this.caseSensitive=options.caseSensitive;
 	this.strict=options.strict;
+	
+}
+
+/**
+ * Route dispatcher the route "middleware"
+ * 
+ * @param {InComMessage} req
+ * @param {serverResponse} res
+ * @param {Function} txt
+ * @api private
+ */
+Router.prototype._dispatch=function(req,res,txt){
 	
 }
